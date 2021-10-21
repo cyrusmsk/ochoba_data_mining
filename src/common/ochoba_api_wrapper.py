ed952d0f3525d4fb74b81a2a76cdef6799562d65
Serg Gini <kornburn@gmail.com>
2021-10-21 23:18:11 +0300

    Change settings to run vc.ru without database

--- src/common/ochoba_api_wrapper.py
+++ src/common/ochoba_api_wrapper.py
@@ -5,7 +5,7 @@ class OchobaApiWrapper:
     def __init__(self, config):
         self.url = config["url"]
         self.session = sessions.BaseUrlSession(base_url=self.url)
-        self.session.headers.update({"X-Device-Token": config["token"]})
+        #self.session.headers.update({"X-Device-Token": config["token"]})
 
     def execute(self, endpoint):
         return self.session.get(endpoint)
